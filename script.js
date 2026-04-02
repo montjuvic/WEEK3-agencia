70edee140d0e0875655c3617d0ec9b3a4493345e
Montjuvic <montjuvic@gmail.com>
2026-04-02 19:33:44 -0300

    Agrego funcionalidad modo oscuro y claro con JAVASCRIPT

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
- 
+// Seleccionamos el buttom y el body
+const toggleBtn = document.getElementById('toggle-theme');
+const body = document.body;
+
+// Verificamos si ya habia una preferencia guardada
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme === 'dark') {
+    body.classList.add('dark');
+    toggleBtn.textContent = 'Modo Claro';
+}
+
+// Evento al hacer clic
+toggleBtn.addEventListener('click', () => {
+    body.classList.toggle('dark');
+
+    // Actualizamos texto boton según el modo activo
+    if (body.classList.contains('dark')) {
+        toggleBtn.textContent = 'Modo Claro';
+        localStorage.setItem('theme', 'dark');
+    } else {
+        toggleBtn.textContent = 'Modo Oscuro';
+        localStorage.setItem('theme', 'light');
+    }
+}); 
